b282866593adfe5348b73dd8b6229833a276150f
liuyingjie <1193418905@qq.com>
2017-12-28 15:57:06 +0800

    拦截器配置

---
+++ 2018/2018_start/farm/src/main/java/com/wx/farm/interceptor/LoginInterceptor.java
@@ -0,0 +1,29 @@
+package com.wx.farm.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Descript: 登陆拦截器
+ * @Author: liuyingjie
+ * @Date: create in 2017/12/28 0028 15:52
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
